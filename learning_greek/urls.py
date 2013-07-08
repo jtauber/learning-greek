c1d711b7ecdf5f6bc6e6966a1365ec8b08004311
James Tauber <jtauber@jtauber.com>
2013-07-08 02:43:29 -0400

    removed unused import

--- learning_greek/urls.py
+++ learning_greek/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
