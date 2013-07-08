e849c66c61ed0f8991e3b93cd120a55d67cf2148
James Tauber <jtauber@jtauber.com>
2013-07-08 01:33:27 -0400

    multi-page surveys

--- learning_greek/activities.py
+++ learning_greek/activities.py
@@ -45,6 +45,65 @@ class DemographicSurvey(object):
         })
 
 
+class DemographicSurvey2(object):
+
+    title = "Demographic Survey"
+    description = "basic demographic questions (over two pages)"
+
+    def __init__(self, activity_state):
+        self.pages = [
+            [
+                {
+                    "name": "birthyear",
+                    "label": "Year of Birth",
+                    "help_text": "what year were you born in?",
+                    "field_class": forms.CharField,
+                }
+            ],
+            [
+                {
+                    "name": "gender",
+                    "label": "Gender",
+                    "field_class": forms.ChoiceField,
+                    "extra_args": {
+                        "choices": [("", ""), ("M", "Male"), ("F", "Female")]
+                    }
+                }
+            ]
+        ]
+        self.activity_state = activity_state
+    
+    def handle_request(self, request):
+        data = self.activity_state.state
+        if not data:
+            data = {"page": 0}
+        elif not data.get("page"):
+            data["page"] = 0
+        elif data["page"] == 2:
+            # done
+            return redirect("dashboard")  # @@@
+        
+        questions = self.pages[data["page"]]
+        
+        if request.method == "POST":
+            form = SurveyForm(request.POST, questions=questions)
+            if form.is_valid():
+                self.activity_state.state.update({"answers_%d" % data["page"]: form.cleaned_data})
+                self.activity_state.state.update({"page": data["page"] + 1})
+                self.activity_state.save()
+                return redirect("activity_play", self.activity_state.activity_slug)
+        else:
+            form = SurveyForm(questions=questions)
+        
+        return render(request, "survey.html", {
+            "title": self.title,
+            "description": self.description,
+            "page_number": data["page"] + 1,
+            "form": form
+        })
+
+
 ACTIVITIES = {
     "demographic": DemographicSurvey,
+    "demographic2": DemographicSurvey2,
 }
