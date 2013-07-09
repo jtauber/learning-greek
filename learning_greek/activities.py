c785efb871a97cb8156db4079dc0a7cbe543a9e1
James Tauber <jtauber@jtauber.com>
2013-07-09 01:36:48 -0400

    prototype of a two-choice quiz

--- learning_greek/activities.py
+++ learning_greek/activities.py
@@ -1,3 +1,7 @@
+# coding: utf-8
+
+import random
+
 from django import forms
 from django.shortcuts import redirect, render
 from django.utils import timezone
@@ -116,7 +120,67 @@ class DemographicSurvey2(MultiPageSurvey):
     ]
 
 
+class UpperCaseQuiz(object):
+    
+    title = "Upper Case"
+    description = "given a lower-case letter, what's the upper-case equivalent"
+    
+    def __init__(self, activity_state):
+        self.activity_state = activity_state
+        
+        if not self.activity_state.state:
+            self.activity_state.state = {"questions": self.construct_quiz()}
+            self.activity_state.save()
+        elif not self.activity_state.state.get("questions"):
+            self.activity_state.state["questions"] = self.construct_quiz()
+            self.activity_state.save()
+    
+    def construct_quiz(self):
+        questions = []
+        letters = u"αβγδεζηθικλμνξοπρστυφχψω"
+        for i in range(10):
+            choices = random.sample(letters, 2)
+            question = random.choice(choices)
+            questions.append((question, [choice.upper() for choice in choices]))
+        return questions
+    
+    def handle_request(self, request):
+        data = self.activity_state.state
+        if not data:
+            data = {"question_number": 0}
+        elif not data.get("question_number"):
+            data["question_number"] = 0
+        elif data["question_number"] == len(data["questions"]):
+            # done
+            return redirect("dashboard")  # @@@
+        
+        question = data["questions"][data["question_number"]]
+        
+        if request.method == "POST":
+            if request.POST.get("question_number") == str(data["question_number"] + 1):
+                answer = request.POST.get("answer")
+                if answer in ["left", "right"]:
+                    self.activity_state.state.update({"answers_%d" % data["question_number"]: answer})
+                    self.activity_state.state.update({"question_number": data["question_number"] + 1})
+                    if data["question_number"] == len(data["questions"]):
+                        self.activity_state.completed = timezone.now()
+                        self.activity_state.save()
+                        return redirect("dashboard")
+                    else:
+                        self.activity_state.save()
+                        return redirect("activity_play", self.activity_state.activity_slug)
+        
+        return render(request, "two_choice_quiz.html", {
+            "title": self.title,
+            "description": self.description,
+            "question_number": data["question_number"] + 1,
+            "num_questions": len(data["questions"]),
+            "question": question,
+        })
+
+
 ACTIVITIES = {
     "demographic": DemographicSurvey,
     "demographic2": DemographicSurvey2,
+    "uppercase": UpperCaseQuiz,
 }
