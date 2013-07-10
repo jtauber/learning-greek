b52f7f35fd9a24326a4c0743d12dbb4e876fe288
James Tauber <jtauber@jtauber.com>
2013-07-10 05:40:28 -0400

    support hyphens in activity slugs

--- learning_greek/activities/urls.py
+++ learning_greek/activities/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns("learning_greek.activities.views",
-    url(r"(?P<slug>\w+)/start/$", "activity_start", name="activity_start"),
-    url(r"(?P<slug>\w+)/play/$", "activity_play", name="activity_play"),
+    url(r"(?P<slug>[\w\-]+)/start/$", "activity_start", name="activity_start"),
+    url(r"(?P<slug>[\w\-]+)/play/$", "activity_play", name="activity_play"),
 )
