e277d6ab33333b1bddf9d6abf0c8c41a364b82a6
James Tauber <jtauber@jtauber.com>
2013-07-24 17:54:55 -0700

    refine noun CNG generation

--- data_import/noun_cng.py
+++ data_import/noun_cng.py
@@ -13,11 +13,37 @@ with open("sblgnt.txt") as f:
                 if "-S" not in robinson and "-C" not in robinson:
                     if "-LI" not in robinson and "-OI" not in robinson:
                         if "-PRI" not in robinson:
-                            if form.endswith("(ν)"):
-                                words[form[:-4]].add(robinson[2:])
-                                words[form[:-4] + "ν"].add(robinson[2:])
-                            else:
-                                words[form].add(robinson[2:])
+                            if robinson[2] != "V":
+                                if form.endswith("(ν)"):
+                                    words[form[:-4]].add(robinson[2:])
+                                    words[form[:-4] + "ν"].add(robinson[2:])
+                                else:
+                                    words[form].add(robinson[2:])
+
+exclude = [
+    "APF|APM",
+    "APF|GSF",
+    "APF|NPF",
+    "APM|NPM",
+    "ASN|NSM|NSN",
+    "DSF|DSM",
+    "DSM|GSM",
+    "GSF|GSM",
+    "GSM|NSF",
+    "NPF|NPM",
+    "NSF|NSM",
+    "ASF|ASM",
+    "ASF|ASN",
+    "ASF|NSF",
+    "ASM|GSM",
+]
+
 
 for word in sorted(words):
-    print word, "|".join(sorted(words[word]))
+    parses = "|".join(sorted(words[word]))
+    if parses not in exclude:
+        if parses in ["APN|NPN", "ASN|NSN"]:
+            parses = "C" + parses[1:3]
+        if parses[2] == "N" and parses[0] in ["N", "A"]:
+            parses = "C" + parses[1:3]
+        print word, parses
