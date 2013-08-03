6397831d7e46686489cf02cbca2e644dc69bbce8
James Tauber <jtauber@jtauber.com>
2013-08-02 22:50:44 -0400

    completion page and result styling (for one quiz)

--- learning_greek/activities/signals.py
+++ learning_greek/activities/signals.py
@@ -3,3 +3,4 @@ import django.dispatch
 
 activity_start = django.dispatch.Signal(providing_args=["slug", "activity_state", "request"])
 activity_play = django.dispatch.Signal(providing_args=["slug", "activity_occurrence_state", "request"])
+activity_completed = django.dispatch.Signal(providing_args=["slug", "activity_occurrence_state", "request"])
