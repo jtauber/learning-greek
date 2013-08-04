534dbeaf283cb80c9f97bf01d334dfb18047c794
James Tauber <jtauber@jtauber.com>
2013-08-04 16:02:47 -0400

    protect against no event

--- oxlos/activities/templatetags/activity_tags.py
+++ oxlos/activities/templatetags/activity_tags.py
@@ -5,10 +5,15 @@ register = template.Library()
 
 @register.assignment_tag
 def get_user_stats_for(user):
+    event_count = user.log_set.count()
+    if event_count:
+        last_event = user.log_set.order_by("-timestamp")[0].timestamp
+    else:
+        last_event = None
     return {
         "username": user.username,
-        "events": user.log_set.count(),
-        "last_event": user.log_set.order_by("-timestamp")[0].timestamp,
+        "events": event_count,
+        "last_event": last_event,
         "adoption_level": user.preference.adoption_level,
         "activities": user.activitystate_set.count(),
         "occurrences": user.activityoccurrencestate_set.count(),
