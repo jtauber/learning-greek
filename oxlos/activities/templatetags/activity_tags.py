161bf7233bed2d7fdff2ab2727187d5f62c77cfd
James Tauber <jtauber@jtauber.com>
2013-08-04 15:35:30 -0400

    show adoption level on staff dashboard

--- oxlos/activities/templatetags/activity_tags.py
+++ oxlos/activities/templatetags/activity_tags.py
@@ -8,6 +8,7 @@ def get_user_stats_for(user):
     return {
         "username": user.username,
         "events": user.log_set.count(),
+        "adoption_level": user.preference.adoption_level,
         "activities": user.activitystate_set.count(),
         "occurrences": user.activityoccurrencestate_set.count(),
         "completed_occurrences": user.activityoccurrencestate_set.filter(completed__isnull=False).count(),
