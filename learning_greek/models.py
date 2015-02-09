6c9537fb8748ecb647dfa4c93d103e2104f448be
Patrick Altman <paltman@gmail.com>
2015-02-08 20:18:23 -0600

    Add preference model and receiver

---
+++ learning_greek/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+
+ADOPTION_LEVEL_CHOICES = [
+    ("bleeding-edge", "Bleeding Edge"),
+    ("early-adopter", "Early Adopter"),
+    ("mainstream", "Mainstream"),
+]
+
+
+class Preference(models.Model):
+    user = models.OneToOneField(User)
+    adoption_level = models.CharField(max_length=20, default="early-adopter", choices=ADOPTION_LEVEL_CHOICES)  # noqa
--- learning_greek/receivers.py
+++ learning_greek/receivers.py
@@ -1,10 +1,21 @@
 from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+from django.contrib.auth.models import User
 
 from account.signals import password_changed
 from account.signals import user_sign_up_attempt, user_signed_up
 from account.signals import user_login_attempt, user_logged_in
 
 from eventlog.models import log
+
+from .models import Preference
+
+
+@receiver(post_save, sender=User)
+def handle_user_save(sender, instance, created, **kwargs):
+    if created:
+        Preference.objects.create(user=instance)
 
 
 @receiver(user_logged_in)
